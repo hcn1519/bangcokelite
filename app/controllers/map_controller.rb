dfdcb137768a84097d98b8c9aa8f2536ce1faefb
Ubuntu <ubuntu@ip-172-31-23-88.ap-northeast-1.compute.internal>
2015-10-29 05:06:50 +0000

    지도 페이지 셋팅

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,16 @@
+class MapController < ApplicationController
+  def mappage
+    @univ = params[:univ]
+
+    @hasuk = HasukHouse.all()
+  end
+
+  def map_detail
+  end
+
+  def from_hasuk
+    @place = HasukHouse.all()
+
+    render json :@place
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   
   
+  post 'map/mappage'
+
+  post 'map/map_detail'
+
+  get 'map/from_hasuk'
+
   resources :hasuk_houses
   root to: 'visitors#index'
   devise_for :users
