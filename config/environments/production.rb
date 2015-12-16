25c666604b07ea9981c9c92132d5f5dc0d169e5b
bigwindow <bigwindow@nate.com>
2015-12-16 06:36:58 +0000

    production asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,16 +20,18 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
   config.assets.precompile += %w( bx_loader.gif controls.png )
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = true
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
