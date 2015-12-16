99c9115ec980ef35c45bce3f4b985c98b290d14e
bigwindow <bigwindow@nate.com>
2015-12-16 04:48:06 +0000

    asset js file setting

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -20,5 +20,4 @@ Rails.application.config.assets.precompile += %w( index_effect.js )
 Rails.application.config.assets.precompile += %w( font-awesome.min.css )
 Rails.application.config.assets.precompile += %w( grayscale.css )
 
-Rails.application.config.assets.precompile += %w( markerwithlabel.js )
 
