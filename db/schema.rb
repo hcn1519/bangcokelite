27efec30251ff0022f8276a242ccb2a80b885e49
bigwindow <bigwindow@nate.com>
2015-12-28 08:48:00 +0000

    schema problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20151228061348) do
 
   create_table "attachments", force: :cascade do |t|
