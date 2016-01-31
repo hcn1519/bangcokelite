6f6a595480a3b1015924ad33ce06c6ac7b4c1f17
bigwindow <bigwindow@nate.com>
2016-01-31 08:17:17 +0000

    asset pipeline images setting

--- app/assets/javascripts/waterfall-light.js
+++ app/assets/javascripts/waterfall-light.js
@@ -5,7 +5,6 @@
  * This source code is licensed under the Apache license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 (function($){
 
 	// waterfall option of every elements
@@ -204,4 +203,3 @@
 	};
 
 })(jQuery);
-
