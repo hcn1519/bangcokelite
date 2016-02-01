5757527bdcaf5b4dc5e6f1a77970141c52207959
bigwindow <bigwindow@nate.com>
2016-02-01 08:44:15 +0000

    login path setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
-
+  
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+  
 end
